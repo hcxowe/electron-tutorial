d374f183c78341a3e40ca54a9c17ca87f0c876ca
hcxowe <hcxowe@126.com>
2018-12-05 14:19:02 +0800

    api test

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -5,10 +5,13 @@ const {
 	BrowserWindow,
 	Menu,
 	MenuItem,
-	globalShortcut
+	globalShortcut,
+	ipcMain,
+	BrowserView
 } = electron
 
 let mainWindow = null
+let child = null
 
 function createWindow() {
 	// 获取屏幕大小
@@ -16,7 +19,10 @@ function createWindow() {
 
 	mainWindow = new BrowserWindow({
 		width: screenWidth,
-		height: screenHeight
+		height: screenHeight,
+		show: false,
+		//backgroundColor: '#2e2c29',
+		title: 'hello'
 	})
 
 	mainWindow.loadFile('index.html')
@@ -26,9 +32,40 @@ function createWindow() {
 	mainWindow.on('closed', function () {
 		mainWindow = null
 	})
+
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show()
+	})
+
+	// 模态子窗口
+	/* child = new BrowserWindow({
+		parent: mainWindow,
+		modal: true,
+		show: false,
+		useContentSize: true, // 使用 web 页面的尺寸
+		center: true, // 窗口在屏幕居中
+		movable: true, // 窗口是否可移动
+		closable: true, // 是否可关闭
+	})
+	child.loadURL('https://github.com')
+	child.once('ready-to-show', () => {
+		child.show()
+	}) */
 }
 
-app.on('ready', createWindow)
+ipcMain.on('loadurl', (event, arg) => {
+	console.log(arg)
+	event.returnValue = 'pong'
+})
+
+app.on('ready', () => {
+	createWindow()
+
+	// 注册全局快捷键
+	globalShortcut.register('CommandOrControl+Y', () => {
+		console.log('CommandOrControl + Y')
+	})
+})
 
 app.on('window-all-closed', function () {
 	if (process.platform !== 'darwin') {
--- electron-quick-start/src/screen.js
+++ electron-quick-start/src/screen.js
@@ -1,10 +1,17 @@
 const electron = require('electron')
 const $ = require('jquery')
 const _ = require('lodash')
+const {
+    ipcRenderer
+} = electron
 
 $(function() {
     $(document).on('mousemove', _.throttle(function(e) {
         let {x, y} = electron.screen.getCursorScreenPoint()
         $('#mouseLabel').text(`x: ${x}, y: ${y}`)
     }, 100))
+
+    $('#btnLoad').click((evt) => {
+        ipcRenderer.sendSync('loadurl', $('#btnLoad').prev().val())
+    })
 })
